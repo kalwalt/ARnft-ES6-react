7401de1688a6a29b391d33025c08b676476a2a00
Walter Perdan <github@kalwaltart.it>
2020-10-09 21:57:46 +0200

    first commit - raw implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ARnft } from '@kalwalt/ar-nft';
+import * as THREE from 'three'
+
+ARnft.init(640, 480, "DataNFT/pinball", 'config.json', true)
+  .then((nft) => {
+    let mat = new THREE.MeshLambertMaterial()
+    let cubeGeom = new THREE.CubeGeometry(1,1,1);
+    let cube = new THREE.Mesh(cubeGeom, mat);
+    cube.position.z = 90;
+    cube.scale.set(180,180,180);
+
+    nft.add(cube);
+  }).catch((error) => {
+    console.log(error);
+  });
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
