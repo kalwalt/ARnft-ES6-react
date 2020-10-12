841c33d40cd801a1daf69d870b32378ab1e0ea71
Walter Perdan <github@kalwaltart.it>
2020-10-12 21:09:04 +0200

    Updates

---
+++ precache-manifest.89cd027224dcbbe19fbd43c1607528e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5942eabe4ae2fb5db44d4cdec3d3b2",
+    "url": "/ARnft-ES6-react/index.html"
+  },
+  {
+    "revision": "3d20bac5185818c0d6ba",
+    "url": "/ARnft-ES6-react/static/css/main.cf394707.chunk.css"
+  },
+  {
+    "revision": "e96da115259277d5bcb0",
+    "url": "/ARnft-ES6-react/static/js/2.49d7b0dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ARnft-ES6-react/static/js/2.49d7b0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d20bac5185818c0d6ba",
+    "url": "/ARnft-ES6-react/static/js/main.ae75d954.chunk.js"
+  },
+  {
+    "revision": "b25d8174dc609ea01c0e",
+    "url": "/ARnft-ES6-react/static/js/runtime-main.c1347a3e.js"
+  }
+]);
