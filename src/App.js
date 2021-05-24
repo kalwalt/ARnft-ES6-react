0019b5731c88c11294c6f89e7cba348594c1cc9b
kalwalt <info@kalwaltart.it>
2021-05-24 17:29:15 +0200

    now it can be loaded without the import issue - issue with Threejs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { ARnft } from '@kalwalt/ar-nft';
-import { ARnftThreejs } from '@webarkit/arnft-threejs';
+import ARnftThreejs from '@webarkit/arnft-threejs';
 import * as THREE from 'three'
 
 ARnft.init(640, 480, "DataNFT/pinball", 'config.json', true)
@@ -12,7 +12,47 @@ ARnft.init(640, 480, "DataNFT/pinball", 'config.json', true)
     cube.position.z = 90;
     cube.scale.set(180,180,180);
 
-    nft.add(cube);
+    let root = new THREE.Object3D();
+    root.matrixAutoUpdate = false;
+    document.addEventListener('containerEvent', function(ev) {
+
+    let canvas = document.getElementById('canvas');
+    let fov = 0.8 * 180 / Math.PI;
+    let ratio = window.clientWidth / window.clientHeight;
+    let config = {
+      "renderer": {
+        "alpha": true,
+        "antialias": true,
+        "context": null,
+        "precision": "mediump",
+        "premultipliedAlpha": true,
+        "stencil": true,
+        "depth": true,
+        "logarithmicDepthBuffer": true,
+        "objVisibility": false
+      },
+      "camera": {
+        "fov": fov,
+        "ratio": ratio,
+        "near": 0.01,
+        "far": 1000
+      }
+    }
+
+    let sceneThreejs = new ARnftThreejs.SceneRendererTJS(config, canvas, root, nft.uuid, true);
+    sceneThreejs.initRenderer();
+
+    let nftAddTJS = new ARnftThreejs.NFTaddTJS(root);
+    nftAddTJS.add(cube);
+
+    const tick = () => {
+      sceneThreejs.draw();
+      window.requestAnimationFrame(tick)
+    }
+    tick()
+
+    })
+
   }).catch((error) => {
     console.log(error);
   });
