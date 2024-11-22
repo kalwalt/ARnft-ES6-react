b519d84bd9a34cbc9744c83105b858163d18f586
Walter Perdan <github@kalwaltart.it>
2024-11-22 19:59:31 +0100

    fix for build action

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        open: './public/index.html',
-    },
     build: {
-        outDir: 'build'
+        outDir: 'build',
+        assetsDir: 'public/src'
     }
 });
